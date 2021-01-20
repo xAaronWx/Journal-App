ebd6f4954cfa6a7184a1a8091a64a0029473a1a2
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-01-19 21:17:55 -0500

    added controllers

--- server-challenge/controllers/animal-controller.js
+++ server-challenge/controllers/animal-controller.js
@@ -1,6 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var sequelize = require('../db');
-var Animal = sequelize.import('../models/animal');
+var sequelize = require("../db");
+var Animal = sequelize.import("../models/animal");
+
+router.post("/create", function (req, res) {
+  const animalCreate = {
+    name: req.body.name,
+    legNumber: req.body.legNumber,
+    predator: req.body.predator,
+  };
+
+  Animal.create(animalCreate)
+    .then((animal) =>
+      res
+        .status(200)
+        .json({ message: "Your animal has been created", data: animal })
+    )
+    .then((err) => res.status(500).json({ error: err }));
+});
+
+router.get("/", (req, res) => {
+  Animal.findAll()
+    .then((animal) => res.status(200).json(animal))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.delete("/delete/:id", function (req, res) {
+  const query = { where: { id: req.params.id } };
+
+  Animal.destroy(query)
+    .then(() => res.status(200).json({ message: "Animal has been deleted" }))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
 module.exports = router;
--- server-challenge/index.js
+++ server-challenge/index.js
@@ -5,12 +5,15 @@ var sequelize = require("./db");
 app.use(express.json());
 
 var user = require("./controllers/user-controller");
+var animal = require("./controllers/animal-controller");
 
 sequelize.sync();
 
 app.use(require("./middleware/headers"));
+app.use(express.json());
 
 app.use("/user", user);
+app.use("/animal", animal);
 
 app.listen(3000, function () {
   console.log("App is listening on port 2999+1");
